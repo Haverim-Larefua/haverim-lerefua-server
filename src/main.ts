daf1dd0b8e6189dfad72fda04164f4a5952d4d18
Zelenovsky, Ivan (iz1155) <iz1155@intl.att.com>
2020-01-29 08:22:58 +0200

    Added cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  Logger.log(`app is listening on port 3001`);
   await app.listen(3001);
 }
 bootstrap();
