cd7283a326a50b65182bbb67a90969ee42e8af9e
Lavi, Israel (il0695) <il0695@intl.att.com>
2020-02-25 11:57:19 +0200

    [1111] Adding utf8 to DB

--- scripts/db-seed/db_seed_script.sql
+++ scripts/db-seed/db_seed_script.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `refua_delivery`;
+CREATE DATABASE IF NOT EXISTS `refua_delivery` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `refua_delivery`;
 
 DROP TABLE IF EXISTS `users`;
