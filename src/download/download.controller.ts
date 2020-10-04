ffc735817f5a0cd7a724b4069f5857648a5e0962
Doron Solomon <doronsolomon@walla.co.il>
2020-10-04 09:54:30 +0300

    Add download URL for iphone and fix the URL for android

--- src/download/download.controller.ts
+++ src/download/download.controller.ts
@@ -4,13 +4,21 @@ import path = require('path');
 
 @Controller('download')
 export class DownloadController {
-    private readonly fileName = "FFH.apk";
+    private readonly fileAPKName = "FFH.apk";
+    private readonly fileIPhoneName = "IPhone.apk";
     private readonly filePath = path.join(__dirname, "../", "assets/downloads");
 
-    @Get()
+    @Get("android")
     downloadAPKFile(@Res() res: Response): void {
         Logger.log(`[DownloadController] downloadAPKFile()`);
         // Todo: handle error
-        res.download(`${this.filePath}/${this.fileName}`);
+        res.download(`${this.filePath}/${this.fileAPKName}`);
+    }
+
+    @Get("iphone")
+    downloadIPhoneFile(@Res() res: Response): void {
+        Logger.log(`[DownloadController] downloadIPhoneFile()`);
+        // Todo: handle error
+        res.download(`${this.filePath}/${this.fileIPhoneName}`);
     }
 }
