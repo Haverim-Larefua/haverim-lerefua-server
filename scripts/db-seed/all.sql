d337034a1b02804af0f4a56894b11f26f64e81a1
Klein, Yanir (yk277u) <yk277u@intl.att.com>
2020-11-22 12:33:25 +0200

    [FORMATION] Update db-ssed all.sql Reviewer: N/A

--- scripts/db-seed/all.sql
+++ scripts/db-seed/all.sql
@@ -1,4 +1,4 @@
-﻿DROP DATABASE `refua_delivery`;
+﻿DROP DATABASE IF EXISTS `refua_delivery`;
 
 CREATE DATABASE IF NOT EXISTS `refua_delivery` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `refua_delivery`;
