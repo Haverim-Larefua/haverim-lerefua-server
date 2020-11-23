18180eadfadcda072b6a0494022f72853cce5925
Klein, Yanir (yk277u) <yk277u@intl.att.com>
2020-11-23 09:19:16 +0200

    Add support for changing parcel status Reviewer: Amit

--- src/parcels/parcels.controller.ts
+++ src/parcels/parcels.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Put, Param, Body, Logger, Delete, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Param,
+  Body,
+  Logger,
+  Delete,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ParcelsService } from './parcels.service';
 import { Parcel } from '../entity/parcel.entity';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,19 +20,27 @@ interface IAddSignatureRequest {
 }
 
 interface IUpdateParcelsStatusRequest {
-  parcels: number[];
+  status: string;
+  userId: number;
+  parcelIds: number[];
 }
 
 export interface IGetAllParcelsQueryString {
-  statusFilterTerm?: ParcelStatus; cityFilterTerm?: string; nameSearchTerm?: string;
+  statusFilterTerm?: ParcelStatus;
+  cityFilterTerm?: string;
+  nameSearchTerm?: string;
 }
 
 @Controller('parcels')
 export class ParcelsController {
-  constructor(private readonly parcelsService: ParcelsService) { }
+  constructor(private readonly parcelsService: ParcelsService) {}
   @Get()
   getAllParcels(@Query() query: IGetAllParcelsQueryString): Promise<Parcel[]> {
-    Logger.log(`[ParcelsController] getAllParcels(), query parameters are: ${JSON.stringify(query)}`);
+    Logger.log(
+      `[ParcelsController] getAllParcels(), query parameters are: ${JSON.stringify(
+        query,
+      )}`,
+    );
     return this.parcelsService.getAllParcels(query);
   }
 
@@ -54,10 +73,17 @@ export class ParcelsController {
     @Param('userId') userId: number,
     @Query() query,
   ): Promise<Parcel[]> {
-    Logger.log(`[ParcelsController] getParcelsByUserId(${userId}, ${JSON.stringify(query)})`);
+    Logger.log(
+      `[ParcelsController] getParcelsByUserId(${userId}, ${JSON.stringify(
+        query,
+      )})`,
+    );
     if (query.last_statuses) {
       const statuses: string[] = query.last_statuses.split(',');
-      return this.parcelsService.getParcelsByUserIdSpecificStatuses(userId, statuses);
+      return this.parcelsService.getParcelsByUserIdSpecificStatuses(
+        userId,
+        statuses,
+      );
     } else {
       return this.parcelsService.getParcelsByUserId(userId);
     }
@@ -80,8 +106,11 @@ export class ParcelsController {
   @Put('assign/:userId')
   assignParcelsToUser(
     @Param('userId') userId: number,
-    @Body() parcelIds: number[]): Promise<Parcel[]> {
-    Logger.log(`[ParcelsController] assignParcelsToUser(${userId}, ${parcelIds})`);
+    @Body() parcelIds: number[],
+  ): Promise<Parcel[]> {
+    Logger.log(
+      `[ParcelsController] assignParcelsToUser(${userId}, ${parcelIds})`,
+    );
     return this.parcelsService.assignParcelsToUser(userId, parcelIds);
   }
 
@@ -89,18 +118,32 @@ export class ParcelsController {
   addParcelSignature(
     @Param('userId') userId: number,
     @Param('parcelId') parcelId: number,
-    @Body() body: IAddSignatureRequest): Promise<Parcel> {
-    Logger.log(`[ParcelsController] addParcelSignature(${userId}, ${parcelId})`);
-    return this.parcelsService.addParcelSignature(userId, parcelId, body.signature);
+    @Body() body: IAddSignatureRequest,
+  ): Promise<Parcel> {
+    Logger.log(
+      `[ParcelsController] addParcelSignature(${userId}, ${parcelId})`,
+    );
+    return this.parcelsService.addParcelSignature(
+      userId,
+      parcelId,
+      body.signature,
+    );
   }
 
   @Put('user/:userId/:status')
   updateParcelsStatus(
     @Param('userId') userId: number,
     @Param('status') status: ParcelStatus,
-    @Body() body: IUpdateParcelsStatusRequest): Promise<number[]> {
-    Logger.log(`[ParcelsController] updateParcelsStatus(${userId}, ${status}, ${body.parcels})`);
-    return this.parcelsService.updateParcelsStatus(userId, status, body.parcels);
+    @Body() body: IUpdateParcelsStatusRequest,
+  ): Promise<number[]> {
+    Logger.log(
+      `[ParcelsController] updateParcelsStatus(${userId}, ${status}, ${body.parcelIds})`,
+    );
+    return this.parcelsService.updateParcelsStatus(
+      body.userId,
+      status,
+      body.parcelIds,
+    );
   }
 
   // @Post()
@@ -121,6 +164,4 @@ export class ParcelsController {
     Logger.log(`[ParcelsController] deleteParcelbyId()`);
     return this.parcelsService.deleteParcel(id, keep);
   }
-
-
 }
