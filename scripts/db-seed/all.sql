b4b66f115a2f0c6b5ea22d63f1ddd74d6f554a04
Doron Solomon <doronsolomon@walla.co.il>
2020-12-06 12:14:58 +0200

    Update the all.sql file Combine the alter to the create Fix bug duplication of exception

--- scripts/db-seed/all.sql
+++ scripts/db-seed/all.sql
@@ -41,9 +41,12 @@ CREATE TABLE `parcel` (
   `address` varchar(100) DEFAULT NULL,
   `phone` varchar(100) DEFAULT NULL,
   `customer_name` varchar(45) DEFAULT NULL,
+  `customer_id` VARCHAR(9) NULL DEFAULT NULL,
   `currentUserId` int(11) DEFAULT NULL,
   `parcelTrackingStatus` varchar(30) DEFAULT NULL,
   `comments` varchar(100) DEFAULT NULL,
+  `start_date` date NULL DEFAULT NULL,
+  `start_time` time NULL DEFAULT NULL,
   `lastUpdateDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `signature` text DEFAULT NULL,
   `deleted` boolean DEFAULT false,
@@ -82,23 +85,26 @@ USE `refua_delivery`$$
 CREATE PROCEDURE `calculateExpiredParcelsSP` ()
 BEGIN
 	DROP TABLE IF EXISTS _temp_exception_ids;
-	CREATE TEMPORARY TABLE _temp_exception_ids(id int, currentUserId int, parcelTrackingStatus VARCHAR(30));
+	CREATE TEMPORARY TABLE _temp_exception_ids(id int, currentUserId int, parcelTrackingStatus VARCHAR(30)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 	INSERT INTO _temp_exception_ids
 	SELECT id, currentUserId, parcelTrackingStatus
 	FROM refua_delivery.parcel
 	WHERE 
+  exception = 0
+  AND
+  (
 	(parcelTrackingStatus = 'ready' AND TIMESTAMPDIFF(HOUR,lastUpdateDate, CURRENT_TIMESTAMP) > 12)
 	OR
     (parcelTrackingStatus = 'assigned' AND TIMESTAMPDIFF(HOUR,lastUpdateDate, CURRENT_TIMESTAMP) > 12)
     OR
-	(parcelTrackingStatus = 'distribution' AND TIMESTAMPDIFF(HOUR,lastUpdateDate, CURRENT_TIMESTAMP) > 6);
-
+	(parcelTrackingStatus = 'distribution' AND TIMESTAMPDIFF(HOUR,lastUpdateDate, CURRENT_TIMESTAMP) > 6)
+  );
 	UPDATE parcel SET lastUpdateDate = CURRENT_TIMESTAMP, exception = 1
 	WHERE id IN (SELECT id from _temp_exception_ids);
 
 	INSERT INTO parcel_tracking (status_date, status, user_fk, parcel_fk, comments)
-	SELECT CURRENT_TIMESTAMP, parcelTrackingStatus, currentUserId, id, "החבילה בחריגה"
+	SELECT CURRENT_TIMESTAMP, parcelTrackingStatus, currentUserId, id, 'החבילה בחריגה'
 	FROM _temp_exception_ids;
 
 	DROP TABLE _temp_exception_ids;
@@ -112,40 +118,10 @@ ON SCHEDULE EVERY 5 MINUTE
 STARTS (TIMESTAMP(CURRENT_TIME))
 DO CALL calculateExpiredParcelsSP;
 
-INSERT INTO `refua_delivery`.`users` (`first_name`, `last_name`, `delivery_area`, `delivery_days`, `phone`, `notes`, `username`, `password`, `salt`, `active`) VALUES ('כהן', 'יוסי', 'באר שבע', '1,2,5', '052-1234567', 'יכול בשעות הערב בלבד', 'rozman', 'd129f44c69f2964cb3768f77af4fac2b95c27183ddc0f36bd76db37cb340f337f8879a588141da6cd79176be6c535aedfa48bf809f7c29229c6033d32ddf4d74', 'b64b7196d89c2f8f', 1);
-INSERT INTO `refua_delivery`.`users` (`first_name`, `last_name`, `delivery_area`, `delivery_days`, `phone`, `notes`, `username`, `password`, `salt`, `active`) VALUES ('לוי', 'יהודית', 'תל אביב', '4,5', '05201233445', 'מגיעה במונית', 'meirav', 'd129f44c69f2964cb3768f77af4fac2b95c27183ddc0f36bd76db37cb340f337f8879a588141da6cd79176be6c535aedfa48bf809f7c29229c6033d32ddf4d74', 'b64b7196d89c2f8f', 1);
-INSERT INTO `refua_delivery`.`users` (`first_name`, `last_name`, `delivery_area`, `delivery_days`, `phone`, `notes`, `username`, `password`, `salt`, `active`) VALUES ('כהנוב', 'בת-אל', 'הרצלייה', '3,4', '050-9235656', 'יכול בערב בלבד', 'harduf', 'd129f44c69f2964cb3768f77af4fac2b95c27183ddc0f36bd76db37cb340f337f8879a588141da6cd79176be6c535aedfa48bf809f7c29229c6033d32ddf4d74', 'b64b7196d89c2f8f', 1);
-INSERT INTO `refua_delivery`.`users` (`first_name`, `last_name`, `delivery_area`, `delivery_days`, `phone`, `notes`, `username`, `password`, `salt`, `active`) VALUES ('יהודה', 'שלום', 'חיפה', '2,3', '054-1888865', '', 'shalom1', 'd129f44c69f2964cb3768f77af4fac2b95c27183ddc0f36bd76db37cb340f337f8879a588141da6cd79176be6c535aedfa48bf809f7c29229c6033d32ddf4d74', 'b64b7196d89c2f8f', 1);
-INSERT INTO `refua_delivery`.`users` (`first_name`, `last_name`, `delivery_area`, `delivery_days`, `phone`, `notes`, `username`, `password`, `salt`, `active`) VALUES ('עברי', 'אהרון', 'עכו', '1,3', '056-1234564', 'יכול בשעות הבוקר בלבד', 'shalom', 'd129f44c69f2964cb3768f77af4fac2b95c27183ddc0f36bd76db37cb340f337f8879a588141da6cd79176be6c535aedfa48bf809f7c29229c6033d32ddf4d74', 'b64b7196d89c2f8f', 0);
-INSERT INTO `refua_delivery`.`users` (`first_name`, `last_name`, `delivery_area`, `delivery_days`, `phone`, `notes`, `username`, `password`, `salt`, `active`) VALUES ('ציפי', 'בראל', 'גדרה', '2,4', '055-1244667', 'פוחדת מכלבים', 'zipi', 'd129f44c69f2964cb3768f77af4fac2b95c27183ddc0f36bd76db37cb340f337f8879a588141da6cd79176be6c535aedfa48bf809f7c29229c6033d32ddf4d74', 'b64b7196d89c2f8f', 1);
-INSERT INTO `refua_delivery`.`users` (`first_name`, `last_name`, `delivery_area`, `delivery_days`, `phone`, `notes`, `username`, `password`, `salt`, `active`) VALUES ('ישראל', 'דוד', 'חולון', '2,3', '053-8872345', 'בשעות הערב בלבד', 'eliyahoo', 'd129f44c69f2964cb3768f77af4fac2b95c27183ddc0f36bd76db37cb340f337f8879a588141da6cd79176be6c535aedfa48bf809f7c29229c6033d32ddf4d74', 'b64b7196d89c2f8f', 1);
-
 INSERT INTO `refua_delivery`.`admins` (`first_name`, `last_name`, `username`, `password`, `salt`) VALUES ('admin', 'admin', 'admin', 'cb9af5d9bb030cd8dc49726670f42401c5b13f2e89d92ca465634e948bbe3c97fd605fc59d4d8df50a427e35166ad3fea241e6ab7b21ed2347e536b96f9e3148', 'd047a22b76dd833f');
-
-
-INSERT INTO `refua_delivery`.`parcel` (`city`, `phone`, `customer_name`, `address`, `currentUserId`, `parcelTrackingStatus`, `comments`, `lastUpdateDate`, `signature`, `deleted`) VALUES ('תל אביב', '052-8556645', 'ישראל ישראלי', 'באר שבע, שטרן 20 א', 14, 'delivered', 'אין הערות', '2020-02-01 08:00:00', 'some base 64 signature', 0);
-INSERT INTO `refua_delivery`.`parcel` (`city`, `phone`, `customer_name`, `address`, `currentUserId`, `parcelTrackingStatus`, `comments`, `lastUpdateDate`, `signature`, `deleted`) VALUES ('באר שבע', '052-1234567', 'יהודית ירושלמי', 'באר שבע, שיכון ד', 14, 'assigned', 'אין הערות', '2020-02-02 08:00:00', 'some base 64 signature', 0);
-INSERT INTO `refua_delivery`.`parcel` (`city`, `phone`, `customer_name`, `address`, `currentUserId`, `parcelTrackingStatus`, `comments`, `lastUpdateDate`, `signature`, `deleted`) VALUES ('באר שבע', '052-1234567', 'ימשה משה', 'באר שבע, שיכון ד', null , 'ready', 'אין הערות', '2020-02-02 08:00:00', 'some base 64 signature', 0);
-
-INSERT INTO `refua_delivery`.`parcel_tracking` (`status_date`, `status`, `user_fk`, `parcel_fk`) VALUES ('2020-02-01 08:00:00', 'delivered', 14, 1);
-INSERT INTO `refua_delivery`.`parcel_tracking` (`status_date`, `status`, `user_fk`, `parcel_fk`) VALUES ('2020-02-01 08:00:00', 'assigned', 14, 2);
-
-INSERT INTO `refua_delivery`.`push_token` (`id`, `user_fk`, `token`) VALUES (1, 14, 'some phone token');
-INSERT INTO `refua_delivery`.`push_token` (`id`, `user_fk`, `token`) VALUES (2, 15, 'some phone token');
-
-
-ALTER TABLE `refua_delivery`.`parcel` 
-ADD COLUMN `start_date` date NULL DEFAULT NULL AFTER `comments`,
-ADD COLUMN `start_time` time NULL DEFAULT NULL AFTER `start_date`;
-
-
-ALTER TABLE `refua_delivery`.`parcel` 
-ADD COLUMN `customer_id` VARCHAR(9) NULL DEFAULT NULL AFTER `customer_name`;
 
 ALTER TABLE `refua_delivery`.`users` 
 ADD FULLTEXT INDEX `search_fulltext` (`first_name`, `last_name`, `phone`) WITH PARSER ngram VISIBLE;
-;
 
 ALTER TABLE `refua_delivery`.`parcel` 
 ADD FULLTEXT INDEX `search_fulltext` (`phone`, `customer_name`, `customer_id`) WITH PARSER ngram VISIBLE;
-;
