abcd2a0fd6f93507aa0fe9b52b10635eed0935f9
PesyaC <pesya.avramovich@gmail.com>
2021-08-18 22:31:45 +0300

    sms

--- src/utils/sms.service.ts
+++ src/utils/sms.service.ts
@@ -3,18 +3,25 @@ import { Logger } from "@nestjs/common";
 export class SMSService {
 
     public static sendSMS(phoneNumber: string, message: string) {
-        const accountSid = 'AC306cdcd8b576a1c245d2f0463dea7b93'; 
-        const authToken = '906b9f41ad8de6cd1d06585be1c81d66'; 
-        //const client = require('twilio')(accountSid, authToken); 
-         
-        // client.messages 
-        //       .create({ 
-        //          body: message,  
-        //          messagingServiceSid: 'MG7e63d55ac2d6ebf739c07f884dd583b4',      
-        //          to: phoneNumber 
-        //        }) 
-        //       .then(message => console.log(message.sid)) 
-        //       .done();
-        Logger.warn(message);
+        try {   
+            // const israelPhoneNumber = "+972" + phoneNumber.substring(1);
+            // const accountSid = "AC4471cb836a4eeb93cfdd16a745f02d56"
+            // const authToken = '772186393d5e8d3be91b1624d9a6fb07';
+            // const client = require('twilio')(accountSid, authToken);
+
+
+            // client.messages
+            //     .create({
+            //         body: message,
+            //         from: '+13522616895',
+            //         to: israelPhoneNumber
+            //     })
+            //     .then(message => console.log(message.sid))
+            //     .done();
+
+            Logger.warn(message);
+        } catch (ex) {
+            Logger.warn(`error send SMS to ${phoneNumber}, error:${ex.message}`)
+        }
     }
 }
