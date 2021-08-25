4d6e33d26a2d6c8e3f59e1aaee6a674da835e311
Avramovich, Pesya(pa7745) <pa7745@intl.att.com>
2021-08-25 10:29:29 +0300

    sms

--- src/utils/sms.service.ts
+++ src/utils/sms.service.ts
@@ -4,20 +4,20 @@ export class SMSService {
 
     public static sendSMS(phoneNumber: string, message: string) {
         try {   
-            // const israelPhoneNumber = "+972" + phoneNumber.substring(1);
-            // const accountSid = "AC4471cb836a4eeb93cfdd16a745f02d56"
-            // const authToken = '772186393d5e8d3be91b1624d9a6fb07';
-            // const client = require('twilio')(accountSid, authToken);
+            const israelPhoneNumber = "+972" + phoneNumber.substring(1);
+            const accountSid = "AC4471cb836a4eeb93cfdd16a745f02d56"
+            const authToken = '772186393d5e8d3be91b1624d9a6fb07';
+            const client = require('twilio')(accountSid, authToken);
 
 
-            // client.messages
-            //     .create({
-            //         body: message,
-            //         from: '+13522616895',
-            //         to: israelPhoneNumber
-            //     })
-            //     .then(message => console.log(message.sid))
-            //     .done();
+            client.messages
+                .create({
+                    body: message,
+                    from: '+13522616895',
+                    to: israelPhoneNumber
+                })
+                .then(message => console.log(message.sid))
+                .done();
 
             Logger.warn(message);
         } catch (ex) {
