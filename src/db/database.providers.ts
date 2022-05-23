11b73c97dac136280c04ea284bff81813f995d5a
PesyaC <74363768+PesyaC@users.noreply.github.com>
2022-05-23 14:15:06 +0300

    hebrew

--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -17,9 +17,6 @@ export const databaseProviders = [
             entities: [__dirname + '/../**/*.entity{.ts,.js}'],
             synchronize: environment.DB.SYNCHRONIZE,
             dateStrings: true,
-            extra: {
-              charset: "utf8mb4_unicode_ci"
-            }
           }),
   },
 ];
